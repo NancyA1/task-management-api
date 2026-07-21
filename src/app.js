ef78f96bbf2ebdd1f2ad9bd965ce5c2944bab4f2
NancyA1 <nancyashraff101@gmail.com>
2026-07-21 16:20:35 +0300

    Add project routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const projectRoutes = require("./routes/projectRoutes")
 const app = express();
+app.use("/api/projects",projectRoutes);
 app.get("/", ( req,res)=> {
     res.json({
      message: "Welcome to the Task Management API"
